1260406e77486240465876a5a3c61109f440e30c
Usman-Nasir94 <usmannasir286@gmail.com>
2026-01-07 18:26:38 +0500

    added multer middelware and cloudinary util to store images and videos on online service

---
+++ src/middlewares/multer.middleware.js
@@ -0,0 +1,18 @@
+import multer from "multer";
+
+
+// this will save the file to a temp folder before uploading to cloudinary for a small period of time
+
+const storage = multer.diskStorage({
+    destination: function (req, file, cb) {
+      cb(null, "./public/temp")
+    },
+    filename: function (req, file, cb) {
+      
+      cb(null, file.originalname)
+    }
+  })
+  
+export const upload = multer({ 
+    storage, 
+});
